b95e7436644ffc0eefcc336a3fe03b47f80f9627
availe <94802525+availe@users.noreply.github.com>
2025-02-18 13:15:33 -0500

    created class

---
+++ Stock_Watcher/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Stock_Watcher;
+
+class Program {
+    private static void Main()
+    {
+        string userInput;
+
+        do
+        {
+            Console.WriteLine("Enter stock ticker: ");
+            string stockTicker = Console.ReadLine();
+
+            Console.WriteLine("Do you wish to add another stock?: ");
+            userInput = Console.ReadLine();
+
+        } while (userInput == "Yes");
+    }
+}
